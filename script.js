a809a7f0c6d83ec8a24554f5833d0b8ca3301d84
Jakub Kopiszka <kopiszkajakub20@gmail.com>
2026-08-19 02:16:04 +0200

    small fixes

--- script.js
+++ script.js
@@ -119,4 +119,9 @@ async function loadPublications() {
 
 loadPublications();
 
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
